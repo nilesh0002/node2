017c732d25d7f8275aa64b81045648d63c7c7334
nilesh0002 <nilesh.singh2024@lpu.in>
2026-04-27 10:07:53 +0530

    do

--- 27april/index.js
+++ 27april/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
-mongoose.connect(``)
+mongoose.connect(`mongodb://localhost:27017/?directConnection=true`)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
